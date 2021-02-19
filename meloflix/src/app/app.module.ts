4f8b404e28e444483ab033088625014cb5cdcacd
Carmelo <melonemark8@gmail.com>
2021-02-19 17:28:28 +0100

    Create first project structure

--- meloflix/src/app/app.module.ts
+++ meloflix/src/app/app.module.ts
@@ -4,16 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './modules/core/home/home.module';
+import { ToolBarComponent } from './shared/components/tool-bar/tool-bar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import {SharedModule} from "./modules/shared/shared.module";
+
+const appComponents = [
+  AppComponent,
+  ToolBarComponent,
+  FooterComponent
+]
+
+const appModules = [
+  BrowserModule,
+  AppRoutingModule,
+  HomeModule,
+  SharedModule
+]
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HomeModule
-  ],
+  declarations: [...appComponents],
+  imports: [...appModules],
   providers: [],
   bootstrap: [AppComponent]
 })
--- meloflix/src/app/modules/core/home/home.module.ts
+++ meloflix/src/app/modules/core/home/home.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContainerComponent } from './components/container/container.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { CardComponent } from './components/card/card.component';
+import { FilmListComponent } from './components/film-list/film-list.component';
+import { SeriesListComponent } from './components/series-list/series-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ContainerComponent, LandingComponent, CardComponent, FilmListComponent, SeriesListComponent],
   imports: [
     CommonModule
   ]
